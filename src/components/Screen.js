ac404a6b7f9f9554dbc0d88c0f1daebff5a4aa3f
Anubhavgarg1910 <utkarshgarg422@gmail.com>
2023-12-25 03:31:00 +0530

    final touch

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,14 +1,10 @@
 import { useContext } from "react";
 import { CalContext } from "../context/CalContext";
-import { Textfit } from "react-textfit";
 
 const Screen = () => {
   const { cal } = useContext(CalContext);
-  return (
-    <Textfit className="screen" max={70} mod="single">
-      {cal.num ? cal.num : cal.res}{" "}
-    </Textfit>
-  );
+
+  return <div className="screen">{cal.num ? cal.num : cal.res}</div>;
 };
 
 export default Screen;
